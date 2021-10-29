50d9bb7d9ea17b280e5b133b0e852d7420391806
Emine Çelik <bilmuhcelikemine@gmail.com>
2021-10-29 11:41:44 +0300

    listeleme

--- Koleksiyonlar/Program.cs
+++ Koleksiyonlar/Program.cs
@@ -66,6 +66,11 @@ namespace Koleksiyonlar
                 Console.WriteLine(sehirler2[i]);
             }
 
+            Console.WriteLine("\n\nşehirler yeni listesi: ");
+            for (int i = 0; i < sehirler2.Length; i++)
+            {
+                Console.WriteLine(sehirler2[i]);
+            }
 
         }
 
