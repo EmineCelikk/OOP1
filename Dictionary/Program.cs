020c92c924a18ad9b1157fde963876c5dbebdc14
Emine Çelik <bilmuhcelikemine@gmail.com>
2021-10-29 13:00:02 +0300

    dictionary çalışması eklendi

---
+++ Dictionary/Program.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Dictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IDictionary<int, string> numberNames = new Dictionary<int, string>();
+
+            numberNames.Add(1, "One"); //adding a key/value using the Add() method
+            numberNames.Add(2, "Two");
+            numberNames.Add(3, "Three");
+            numberNames.Add(4, "four");
+
+            foreach (KeyValuePair<int,string> item in numberNames)
+            {
+                Console.WriteLine(item.Key+" - "+item.Value);
+            }
+
+
+            Console.WriteLine("\n*****************************\n");
+            MyDictList<int, string> numberNames2 = new MyDictList<int, string>();
+            numberNames2.Add(3, "three");
+            numberNames2.Add(2, "Two");
+            numberNames2.Add(3, "Three");
+            numberNames2.Add(4, "four");
+
+            numberNames2.Getir();
+
+            
+
+        }
+    }
+
+    class MyDictList<K, V>
+    {
+        K[] diziKey;
+        V[] diziValue;
+
+        K[] diziKeyTemp;
+        V[] diziValueTemp;
+
+
+        public MyDictList()
+        {
+            diziKey = new K[0];
+            diziValue = new V[0];
+
+        }
+        public void Add(K item1,V item2)
+        {
+            diziKeyTemp = diziKey;
+            diziValueTemp = diziValue;
+
+            diziKey = new K[diziKey.Length + 1];
+            diziValue = new V[diziValue.Length + 1];
+
+            for (int i = 0; i < diziKeyTemp.Length; i++)
+            {
+                diziKey[i] = diziKeyTemp[i];
+                diziValue[i] = diziValueTemp[i];
+
+            }
+            diziKey[diziKey.Length - 1] = item1;
+            diziValue[diziValue.Length - 1] = item2;
+
+        }
+        
+
+        public void Getir()
+        {
+            for (int i = 0; i < diziValue.Length; i++)
+            {
+                Console.WriteLine(diziKey[i]+" - "+diziValue[i]);
+            }
+        }
+
+    }
+
+
+}
---
+++ Generics/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Generics
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            List<string> sehirler = new List<string>();
+            sehirler.Add("ankara");
+
+            MyList<string> sehirler2 = new MyList<string>();
+            sehirler2.Add("adana");
+
+        }
+
+
+        class MyList<T>
+        {
+            T[] dizi;
+            
+            public MyList()
+            {
+                dizi = new T[0];
+
+            }
+
+            public void Add(T item)
+            {
+
+                T[] geciciDizi = dizi;
+                dizi = new T[dizi.Length + 1];
+
+                for (int i = 0; i < geciciDizi.Length; i++)
+                {
+                    geciciDizi[i] = dizi[i];
+                }
+
+                dizi[dizi.Length - 1] = item;
+
+            }
+
+
+        }
+
+    }
+}
